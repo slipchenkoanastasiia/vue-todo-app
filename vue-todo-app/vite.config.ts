cda793f13138f1f0b3f3e82a4529bcec672bdaed
slipchenkoanastasiia <138067087+slipchenkoanastasiia@users.noreply.github.com>
2025-05-28 20:23:58 +0300

    add yml

--- vue-todo-app/vite.config.ts
+++ vue-todo-app/vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/vue-todo-app/',   
-  plugins: [vue()],
+  base: '/vue-todo-app/', 
+  plugins: [vue()]
 })
